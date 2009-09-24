28f44d7b17a67b6209bf65005b749edc6a2e346f
konk303 <konk303@gmail.com>
2009-09-24 10:51:42 +0900

    static pages, first commit

--- app/ln.rb
+++ app/ln.rb
@@ -22,24 +22,29 @@ configure do
   #static files are served from gae static options
   disable :static
 #   disable :dump_errors
+  #haml options
+  set :haml, { :format => :html5}
 end
 
 configure :development do
-  @config_from_file['development'].each {|key,val| set key.to_sym => val}
-#reloading, http://groups.google.com/group/appengine-jruby/browse_thread/thread/dbf5cb3c9b7e3ff6/04ba4409cfc9c8b4?lnk=gst&q=shotgun#04ba4409cfc9c8b4
+  @config['development'].each {|key,val| set key.to_sym => val}
+  #reloading, http://groups.google.com/group/appengine-jruby/browse_thread/thread/dbf5cb3c9b7e3ff6/04ba4409cfc9c8b4?lnk=gst&q=shotgun#04ba4409cfc9c8b4
   require 'lib/reloader'
   use Sinatra::Reloader
   use Sinatra::ShowExceptions
 end
 
 configure :production do
-  @config_from_file['production'].each {|key,val| set key.to_sym => val}
+  @config['production'].each {|key,val| set key.to_sym => val}
 end
 
 
 
 #before filter
 before do
+  @body_id = request.path_info.split('/')[1] || 'index'
+  @page_title = " - mixiアプリ「リスニンなう」"
+  @description = "opensocial gadget / mixiアプリ 「リスニンなう」の紹介です。"
 #  Log.warn [options.environment,options.last_fm['api_key']].join(", ")
 end
 
@@ -47,8 +52,54 @@ end
 
 #controllers
 get '/' do
-  hoge = "hoge"
-  haml '%h1 Hello, world! #{:hoge} env:#{options.root}', :locals => {:hoge => hoge}
+  @page_title[0,0] = "listenin' now"
+  haml :index, :locals => {
+  }
+end
+#fix me. どのページでも動くようにする
+get '/index.html' do
+  redirect '/', 301
+end
+
+get '/help' do
+  @page_title[0,0] = "ヘルプ"
+  haml @body_id.intern
+end
+
+get '/demo' do
+  @page_title[0,0] = "デモ"
+  haml @body_id.intern
+end
+
+get '/updates' do
+  @page_title[0,0] = "更新履歴"
+  # todo: datastore使ってcms化
+  haml :updates, :locals => {
+    :updates => [
+                 {:date => Time.local(2009,9,25),
+                   :title =>'説明サイトをオープンしました',
+                   :content => <<"EOF"
+<p>説明サイトを作成し、オープンしました。</p>
+EOF
+                 },
+                 {:date => Time.local(2009,9,25),
+                   :title =>'サーバーを移転しました',
+                   :content => <<"EOF"
+<p>google app engine上にサーバーを移転しました。</p>
+EOF
+                 },
+                ]
+  }
+end
+
+get '/feedback' do
+  @page_title[0,0] = "フィードバック"
+  haml @body_id.intern
+end
+
+get '/tech' do
+  @page_title[0,0] = "技術情報"
+  haml @body_id.intern
 end
 
 get '/api/lastfm' do
@@ -73,8 +124,4 @@ get '/admin/:path' do
   if 'cron/memcache_stat'
     Memcache.new.log_stat
   end
-end
-
-get '/hi/:name' do
-  haml '%h1 Hello, #{params[:name]}!'
 end
